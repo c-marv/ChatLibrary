6b928dca39e540064847e4f6165385f68c65ba62
YersonKira <yerson_kira@hotmail.com>
2014-02-03 21:09:37 -0400

    Commit

---
+++ ClientSide/src/model/Program.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import common.Message;
+import common.MessageEvent;
+import common.MessageListener;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		User user = new User("yerson", "localhost", 8000);
+		user.AddMessageListener(new MessageListener() {
+			@Override
+			public void MessageReceived(MessageEvent e) {
+				System.out.println("New Message: " + e.getMessage().getText());
+			}
+		});
+		user.StartReadMessages();
+		while (true) {
+			System.out.print(">> ");
+			String message = input.nextLine();
+			user.WriteOutputMessage(new Message("ALL", message));
+		}
+	}
+}
---
+++ ClientSide/src/model/User.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+import common.Client;
+import common.Message;
+import common.UserInformation;
+
+public class User extends Client{
+	private Socket socket;
+	public User(String username, String serverip, int port) {
+		this.Initialize(username, serverip, port);
+	}
+	public void Initialize(String username, String server, int port) {
+		try {
+			this.socket = new Socket(server, port);
+			this.input = new Scanner(this.socket.getInputStream());
+			this.output = new PrintStream(this.socket.getOutputStream());
+			this.userInformation = new UserInformation(InetAddress.getLocalHost().getHostAddress(), username);
+			this.WriteOutputMessage(new Message("SERVER", username));
+			this.StartReadMessages();
+		} catch (Exception e) {
+			
+		}
+	}
+}
